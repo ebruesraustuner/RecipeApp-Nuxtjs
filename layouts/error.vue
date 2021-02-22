8596f27f60e7213cf676963703f58480e2a8d5b3
ebruesraustuner <ebruesra06@gmail.com>
2021-02-22 12:53:11 +0300

    Added  Error

---
+++ layouts/error.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="error">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+
+</script>
+<style scoped lang="scss">
+  .error {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    position: relative;
+
+    &::before {
+      display: block;
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 0;
+      z-index: 1;
+      background-image: url(../assets/error-colored.svg);
+      width: 100%;
+      height: 100%;
+      background-repeat: no-repeat;
+      background-position: 50% 0;
+      background-size: center;
+      opacity: .4;
+    }
+
+    h1 {
+      z-index: 1;
+    }
+
+    a {
+      z-index: 1;
+      text-decoration: none;
+      font-size: 18px;
+      color: $lightbrown;
+
+      &:visited {
+        background-color: $pink;
+      }
+
+      &:hover {
+        color: $pink;
+      }
+    }
+
+  }
+
+</style>
